219c478a4e147b791e017aabea3b6149c87a0404
lmoreno <luisku@comunidad.unam.mx>
2024-06-28 00:06:30 -0600

    Ahora se puede iniciar sesión con correo o teléfono, no se necesita registrar ambas cosas a la hora de registrar al empleado

--- api/controllers/branch.report.controller.js
+++ api/controllers/branch.report.controller.js
@@ -210,13 +210,22 @@ export const updateBranchReport = async (req, res, next) => {
 export const getBranchReport = async (req, res, next) => {
 
   const branchId = req.params.branchId
-  const date = (new Date((new Date(req.params.date)).toLocaleDateString('en-us'))).toISOString()
+  const date = new Date(req.params.date)
 
+  const actualLocaleDate = new Date(new Date(date).getTime() - 6 * 60 * 60000)
+  const actualLocaleDay = actualLocaleDate.toISOString().slice(0, 10)
+
+  const actualLocaleDatePlusOne = new Date(actualLocaleDay)
+  actualLocaleDatePlusOne.setDate(actualLocaleDatePlusOne.getDate() + 1)
+  const actualLocalDayPlusOne = actualLocaleDatePlusOne.toISOString().slice(0, 10)
+
+  const bottomDate = new Date(actualLocaleDay + 'T00:00:00.000-06:00')
+  const topDate = new Date(actualLocalDayPlusOne + 'T00:00:00.000-06:00')
   console.log(date)
 
   try {
 
-    const originalBranchReport = await fetchBranchReport(branchId, date)
+    const originalBranchReport = await fetchBranchReport(branchId, bottomDate)
 
     if (originalBranchReport) {
 
