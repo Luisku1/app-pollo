ff819dd82e8d50f1022b9345c12a688bf9df5d93
lmoreno <luisku@comunidad.unam.mx>
2024-09-06 19:23:52 -0600

    Se corrigió el problema de pago a empleados, ya que no eliminaba el efectivo correctamente modificando la cuenta en la que fue registrado, además de corregir la presentación de reportes de empleados, mostrando justo los de la semana en curso

--- api/utils/formatDate.js
+++ api/utils/formatDate.js
@@ -1,6 +1,6 @@
 export const formatDate = (date) => {
 
-  const actualLocaleDate = new Date(date)
+  const actualLocaleDate = convertTZ(new Date(date))
 
   return (actualLocaleDate.getFullYear() + '-' + (actualLocaleDate.getMonth() < 9 ? '0' + ((actualLocaleDate.getMonth()) + 1) : ((actualLocaleDate.getMonth()))) + '-' + ((actualLocaleDate.getDate() < 10 ? '0' : '') + actualLocaleDate.getDate()) + 'T06:00:00.000Z')
 }
