c61a788c394551a4348be9ab023c6073c72beba2
lmoreno <luisku@comunidad.unam.mx>
2025-03-13 10:32:07 -0600

    pequeño fix del fix

--- client/src/services/employees/getEmployeeUser.js
+++ client/src/services/employees/getEmployeeUser.js
@@ -1,6 +1,6 @@
 export const getSignedUser = async (employeeId) => {
   try {
-    const res = await fetch.get(`/api/employees/${employeeId}`)
+    const res = await fetch.get(`/api/employeee/${employeeId}`)
     const data = await res.json()
     return data.data
   } catch (error) {
